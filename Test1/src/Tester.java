618202c7936b779a6a27ecb57a677847285fa093
SAYANTAN DEY <SAYANTAN DEY@Lenovo-PC>
2022-02-10 16:18:47 +0530

    Updated code in Tester.java

--- Test1/src/Tester.java
+++ Test1/src/Tester.java
@@ -1,4 +1,23 @@
 
 public class Tester {
 
+	
+	public static void main (String args[])
+	
+	
+	{
+		//Tester test=new Tester();
+	HelloWorld();
+	}
+	
+	static void HelloWorld()
+	
+	{
+		System.out.println("Hello World");
+	}
+	
+	
 }
+
+
+
